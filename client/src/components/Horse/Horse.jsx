90383f12bc4747471949774de635200b6ddec6ca
LDanchenko <2l-u-b-o-v@ukr.net>
2022-08-10 17:22:40 +0300

    Added horse components with transition

--- client/src/components/Horse/Horse.jsx
+++ client/src/components/Horse/Horse.jsx
@@ -1,15 +1,30 @@
 import style from "./Horse.module.css";
 import horse from "./horse.png";
+import { useRef, useEffect } from "react";
 
-const Horse = ({ left = { distance: 0 } }) => {
-  console.log(left.distance);
+const Horse = ({ left, c }) => {
+  const distance = left * c;
+  const hex = Math.floor(Math.random() * 16777215).toString(16);
+  const containerColor = useRef(hex);
+  useEffect(() => {
+    containerColor.current = hex;
+  }, []);
+
   return (
-    <div className={style.line} align="center" size="2" color="#ff0000">
+    <div
+      className={style.line}
+      style={{
+        backgroundColor: `#${containerColor.current}`,
+      }}
+      align="center"
+      size="2"
+      color="#ff0000"
+    >
       <img
         src={horse}
         width="100px"
         style={{
-          transform: `translateX(${left.distance}px) translateY(-50%)`,
+          transform: `translateX(${distance}px) translateY(-50%)`,
         }}
         alt="horse"
         className={style.image}
